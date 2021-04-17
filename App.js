a62f0f96a6eb87470489867deb882c7e4630c966
Naveen <77867722+thephotogenicbug@users.noreply.github.com>
2021-04-17 12:53:26 +0530

    SlateMate-Source: update

--- App.js
+++ App.js
@@ -1,25 +1,77 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Grid, Paper, Typography, CardMedia} from '@material-ui/core';
+import {Grid, Paper, Typography, CardMedia, Hidden} from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 
 const useStyles = makeStyles((theme) => ({
+
   grid:{
     width:'100%',
     marginTop:'2%',
-   
-    
+     
   },
   paper:{
     padding:theme.spacing(2),
     textAlign:'center',
-  },
-  media:{
-    height:'400px',
-    margin:'auto',
-    width:'400px',
+    background:'#181357',
+    [theme.breakpoints.down("sm")]:{
+      
+    }
 
   },
+  media:{
+    height:'350px',
+    margin:'auto',
+    width:'350px',
+    [theme.breakpoints.down("sm")]:{
+      width:'250px',
+      height:'250px',
+      
+    }
+
+  },
+  heading:{
+    color:'#eb59ae',
+     fontWeight: 'bold', 
+     fontSize:'60px',
+     textAlign:'justify', 
+     margin:'5%', 
+     backgroundSize:'100%',
+     [theme.breakpoints.down("sm")]:{
+       textAlign:"center",
+       fontSize:'45px',
+       margin:'3%', 
+
+     }
+  },
+  paragraph:{
+    color:'#ffff', 
+    fontWeight: 'bold', 
+    fontSize:'30px', 
+    textAlign:'justify', 
+    margin:'5%',
+    [theme.breakpoints.down("sm")]:{
+      fontSize:'20px',
+      margin:'1%',
+      textAlign:'center',
+  
+
+    }
+    
+  },
+  threegrid:{
+    fontWeight:'bold',  
+     margin:'auto',
+     fontSize:'25px',
+     marginTop:'5%',
+     [theme.breakpoints.down("sm")]:{
+      fontWeight:'bold',  
+      marginTop:'5%',
+      fontSize:'15px',
+     }
+  }
  
 
 }));
@@ -28,37 +80,44 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
+
   return (
     <Grid container spacing={0} className={classes.grid} >
-      <Grid item xs={12} md={6} >
-        <Paper className={classes.paper} style={{background:'#181357' ,  }}>
-        <Typography variant="h4" style={{color:'#eb59ae', fontWeight: 'bold', fontSize:'60px',textAlign:'justify', margin:'5%', backgroundSize:'100%' }}>
+      <Grid item xs={12} md={6}   >
+        
+        <Paper className={classes.paper} >
+        <Typography className={classes.heading}  variant="h4" >
         One-to-one online<br/> tuitions from home
+       
         </Typography>
-        <Typography variant="h5" style={{color:'#ffff', fontWeight: 'bold', fontSize:'30px', textAlign:'justify', margin:'5%'}}>
+        <Typography  className={classes.paragraph}  variant="h5" >
         Keep your child’s studies on track with <br/> interactive online tuition
         </Typography>
+       
         </Paper>
+        
       </Grid>
       <Grid item xs={12} md={6} >
           <CardMedia className={classes.media} image="student.png" />
       </Grid>
       <Grid item xs={6} md={4}   container  justify="center" alignItems="flex-start">
-         <Typography  variant={'subtitle1'}  style={{fontWeight:'bold',   margin:'auto'}}>
+         <Typography   className={classes.threegrid}  variant={'subtitle1'} >
          40000+ Tutoring hours   
          </Typography>
       </Grid>
       <Grid item xs={6} md={4}  container  justify="center" alignItems="flex-start" >
-         <Typography  variant={'subtitle1'} style={{fontWeight:'bold',  margin:'auto'}}>
+         <Typography className={classes.threegrid} variant={'subtitle1'} >
          5-Star G o o g l e  Reviews
   
          </Typography>
       </Grid>
+      <Hidden xsDown>
       <Grid item xs={6} md={4}  container justify="center" alignItems="flex-start"  >
-         <Typography  variant={'subtitle1'} alignItems="flex-start"  style={{fontWeight:'bold',  margin:'auto'}}>
+         <Typography   className={classes.threegrid}  variant={'subtitle1'} alignItems="flex-start">
          Trusted by 3000+ Students  
          </Typography>
       </Grid>
+      </Hidden>
     </Grid>
   );
 }
